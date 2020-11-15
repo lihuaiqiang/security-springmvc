e66466097fba9c0005ebf9d8fb2b729403aacc0d
lihq <lihq@primeton.com>
2020-11-15 13:38:27 +0800

    实现会话功能

--- src/main/java/com/itheima/security/springmvc/controller/LoginController.java
+++ src/main/java/com/itheima/security/springmvc/controller/LoginController.java
@@ -4,8 +4,11 @@ import com.itheima.security.springmvc.mode.AuthenticationRequest;
 import com.itheima.security.springmvc.mode.UserDto;
 import com.itheima.security.springmvc.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author lihuaiqiang
@@ -19,8 +22,29 @@ public class LoginController {
     private AuthenticationService authenticationService;
 
     @PostMapping(value = "/login", produces = "text/plain;charset=utf-8")
-    public String login(AuthenticationRequest authenticationRequest) {
+    public String login(AuthenticationRequest authenticationRequest, HttpSession session) {
         UserDto userDto = authenticationService.authentication(authenticationRequest);
+        //将用户信息放入到 session 中
+        session.setAttribute(UserDto.SESSION_USER_KEY, userDto);
         return userDto.getUsername() + "登陆成功";
+    }
+
+    @GetMapping(value = "/logout", produces = "text/plain;charset=utf-8")
+    public String logout(HttpSession session){
+        //用户退出登录状态后，清空session中的用户信息
+        session.invalidate();
+        return "用户退出";
+    }
+
+    @GetMapping(value = "/r/r1",produces = {"text/plain;charset=utf-8"})
+    public String r1(HttpSession session) {
+        String fullname = null;
+        Object attribute = session.getAttribute(UserDto.SESSION_USER_KEY);
+        if (null != attribute) {
+            fullname = ((UserDto) attribute).getFullname();
+        } else {
+            fullname = "匿名";
+        }
+        return fullname + "：访问资源1";
     }
 }
